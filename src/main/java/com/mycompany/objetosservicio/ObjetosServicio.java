b85b3e59591bbe06fb20c9ab3f8528fe10e3c4d7
mildreth <>
2026-04-29 14:20:56 -0700

    se creo el proyecto y las clases

---
+++ src/main/java/com/mycompany/objetosservicio/ObjetosServicio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.objetosservicio;
+
+/**
+ *
+ * @author mildr
+ */
+public class ObjetosServicio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
